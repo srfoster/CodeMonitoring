775af00e6ce7ad3a4256d7fc4234a760cd6412a1
stephenfoster <stephenfoster@192.168.0.104>
2012-01-03 19:56:34 -0800

    Test commit

--- src/com/example/helloandroid/HelloAndroid.java
+++ src/com/example/helloandroid/HelloAndroid.java
@@ -3,6 +3,8 @@ package com.example.helloandroid;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+//This comment is a test.
 public class HelloAndroid extends Activity {
     /** Called when the activity is first created. */
     @Override
